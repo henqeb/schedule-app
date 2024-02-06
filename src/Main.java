6d67f7a6efcf12a6aa0892f03a7af218bd1145d5
henqeb <henrikbellsund@outlook.com>
2024-02-06 14:13:15 +0100

    Add Scheduler class

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,18 @@ public class Main {
         List<Person> personList = new ArrayList<>();
         Person p1 = new Person("Henrik", "henrik@mail.com");
         Person p2 = new Person("Kirneh", "kirneh@mail.com");
-        Person p3 = new Person("Alice", "kirneh@mail.com");
-        Person p4 = new Person("Bob", "kirneh@mail.com");
-        Person p5 = new Person("Charlie", "kirneh@mail.com");
+        Person p3 = new Person("Alice", "alice@mail.com");
+        Person p4 = new Person("Bob A.", "bob@mail.com");
+        Person p5 = new Person("Charlie", "charlie@mail.com");
+        Person p6 = new Person("Bob B.", "bob@mail.com"); // test duplicate mails
         personList.add(p1);
         personList.add(p2);
         personList.add(p3);
         personList.add(p4);
         personList.add(p5);
+        personList.add(p6);
+
+        Scheduler scheduler = new Scheduler(personList);
 
         Meeting m1 = new Meeting(personList, 11);
     }
---
+++ src/Scheduler.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+/**
+ * Functions as "main system" for the scheduling app.
+ */
+public class Scheduler {
+
+    private List<Person> personList;
+
+    public Scheduler(List<Person> personList) {
+        this.personList = personList;
+    }
+}
