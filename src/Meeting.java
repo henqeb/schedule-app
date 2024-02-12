45cd761cf324f15f3319056bf13142e25b4bf31f
henqeb <henrikbellsund@outlook.com>
2024-02-12 12:57:07 +0100

    Refactor representation of time for Meeting class

--- src/Meeting.java
+++ src/Meeting.java
@@ -1,14 +1,15 @@
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Meeting {
     
     private List<Person> participants;
-    // using an integer to represent time, as meetings can only start at the hour mark and last one hour.
-    // e.g., 14:00 is represented as 14, 08:00 as 8, etc.
-    private int startingTime;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 
-    public Meeting(List<Person> participantList, int startingTime) {
+    public Meeting(List<Person> participantList, LocalDateTime startTime, LocalDateTime endTime) {
         this.participants = participantList;
-        this.startingTime = startingTime;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 }
