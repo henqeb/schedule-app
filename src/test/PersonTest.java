cfc14c75dc7b35bfd6b149dc319d39a00031af8a
henqeb <henrikbellsund@outlook.com>
2024-03-02 17:26:53 +0100

    Test

--- src/test/PersonTest.java
+++ src/test/PersonTest.java
@@ -3,10 +3,17 @@ package test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.*;
+import java.*; // "local" java package (bad naming practices)
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class PersonTest {
     
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
     Person person;
 
     @BeforeEach
@@ -16,7 +23,11 @@ public class PersonTest {
 
     @Test
     void whenGetPersonsSchedule_thenShouldNotBeEqual() {
-        ;
+        // add schedule to Person
+        List<Meeting> listToAdd = new ArrayList<>();
+        LocalDateTime m5startTime = LocalDateTime.parse("12.01.2024 15:45", formatter);
+        LocalDateTime m5endTime = LocalDateTime.parse("12.01.2024 16:15", formatter);
+        Meeting m = new Meeting(Arrays.asList(person), null, null);
     }
     
 }
